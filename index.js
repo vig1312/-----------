3eed1810d0f347c8991efa10a9c6873da780f0e1
Vigen_vardanyan <vigen.vardanyan.06@gmail.com>
2024-05-08 15:06:23 +0400

    added new promise

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function sayHi() {
 for(let i = 0;i < 10;i++) {
     console.log(i)
 }
+
+let data = [
+    {
+        name: "vigen",
+        lastName:"Vardanyan"
+    }
+]
+
+function newPromise(data) {
+    return new Promise(function(resolve,reject) {
+        setTimeout(function() {
+            data.forEach((member,index) => {
+                if(member.name.toLowerCase().includes("Vigen")) {
+                    resolve(`Hello ${member.name} in ${index} index`)
+                } else {
+                    reject(`not found ${index} index`)
+                }
+            })
+        })
+    })
+}
+
+let promise = newPromise(data)
+
+promise.ther(function(result) {
+    console.log(result)
+}).catch(function(result) {
+    console.log(result)
+})
